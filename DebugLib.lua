92d58ec14ab330ca6e345432bc5076765635f9e4
trippedBit <130838437+trippedBit@users.noreply.github.com>
2023-12-28 11:06:48 +0100

    feat: DebugMessage supports replacements

--- DebugLib.lua
+++ DebugLib.lua
@@ -6,10 +6,17 @@
 ---------------------------------------------------------------------------------
 -- Shows the provided message as info text (top center) if debug mode is enabled.
 -- pText: String ID in user_strings.res
+-- pReplacement: Replacement string ID in user_strings.res
 ---------------------------------------------------------------------------------
-DebugMessage = function(pText)
+DebugMessage = function(pText, pReplacement)
 	if "true" == MissionLib.GetChapterState("DebugModeSwitchENABLE", 1) then
-		NGUI.ShowInfoText({Text=pText})
+		if (pReplacement ~= nil) then
+			NGUI.ShowInfoText({
+				Text = pText,
+				Replace = {pReplacement}})
+		else
+			NGUI.ShowInfoText({Text = pText})
+		end
 	end
 	return nil
 end
