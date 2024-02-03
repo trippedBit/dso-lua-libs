c9032e0b2a183f5abdf72e554cca147577f0b574
trippedBit <130838437+trippedBit@users.noreply.github.com>
2024-02-03 15:07:26 +0100

    feat: added namespace

--- DebugLib.lua
+++ DebugLib.lua
@@ -3,12 +3,16 @@
 -- Copyright (c) 2023 trippedBit
 ---------------------------------
 
+import "string"
+
+DL = {}  -- Table for this lib
+
 ---------------------------------------------------------------------------------
 -- Shows the provided message as info text (top center) if debug mode is enabled.
 -- pText: String ID in user_strings.res
 -- pReplacement: Replacement string ID in user_strings.res
 ---------------------------------------------------------------------------------
-DebugMessage = function(pText, pReplacement)
+DL.ShowMessage = function(pText, pReplacement)
 	if "true" == MissionLib.GetChapterState("DebugModeSwitchENABLE", 1) then
 		if (pReplacement ~= nil) then
 			NGUI.ShowInfoText({
@@ -18,5 +22,29 @@ DebugMessage = function(pText, pReplacement)
 			NGUI.ShowInfoText({Text = pText})
 		end
 	end
+	return nil
+end
+
+---------------------------------------------------------------------------------
+-- Returns varToCheck's data type as string.
+-- varToCheck: Parameter to check
+---------------------------------------------------------------------------------
+DL.GetType = function(varToCheck)
+	local dataType = lua.type(varToCheck)
+	return string.toupper(dataType)
+end
+
+---------------------------------------------------------------------------------
+-- Shows the datatype of the provided game logic string.
+-- sLogicData: Game logic string, e.g. "Logic.PlayerShip"
+---------------------------------------------------------------------------------
+DL.ShowDataType = function(sLogicData)
+	local logicData = get(sLogicData)
+	local dataType = DL.GetType(logicData)
+
+	NGUI.ShowInfoText({
+		Text = "ID_DATATYPE",
+		Replace = { "ID_DATATYPE_"..dataType }
+	})
 	return nil
 end
